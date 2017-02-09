5d67530a0d0e1b1ac75348aa6e8993e11cff193b
Sandeep <stengali33@gmail.com>
2017-02-08 21:08:58 -0800

    FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sandeep on 2/8/17.
+ */
+public class FizzBuzz {
+
+    private static List<String> fizzBuzz(int n) {
+
+        if(n<1){
+            return null;
+        }
+        
+        List<String> output = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3 == 0 && i%5 == 0)
+                output.add("FizzBuzz");
+            else if(i%3 == 0)
+                output.add("Fizz");
+            else if(i%5 == 0)
+                output.add("Buzz");
+            else
+                output.add(String.valueOf(i));
+        }
+        return output;
+    }
+
+    public static void main(String args[]){
+        List<String> fizzBuzzWords = fizzBuzz(15);
+
+        if(fizzBuzzWords != null && fizzBuzzWords.size() > 0) {
+            for (String str : fizzBuzzWords) {
+                System.out.println(str);
+            }
+        }
+
+    }
+}
+
